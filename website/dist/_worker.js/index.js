f74dfeaa70c03b22e0a2bbd7d49563d2b2731b99
frankeluoregon <123989822+frankeluoregon@users.noreply.github.com>
2026-02-17 14:49:49 -0800

    hero

--- website/dist/_worker.js/index.js
+++ website/dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_D5e-qO8O.mjs';
-import { manifest } from './manifest_C_rp71Nj.mjs';
+import { manifest } from './manifest_CtQ4BNKm.mjs';
 
 const serverIslandMap = new Map();;
 
