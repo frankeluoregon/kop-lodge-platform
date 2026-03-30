8e1df72a04a83dae66e7135fd8cdf72d1ecd860c
frankeluoregon <123989822+frankeluoregon@users.noreply.github.com>
2026-03-29 20:00:11 -0700

    redir

--- website/dist/_worker.js/index.js
+++ website/dist/_worker.js/index.js
@@ -1,7 +1,7 @@
 globalThis.process ??= {}; globalThis.process.env ??= {};
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_BQri0mMk.mjs';
-import { manifest } from './manifest_CNcaBOFr.mjs';
+import { manifest } from './manifest_BXXh92hZ.mjs';
 
 const serverIslandMap = new Map();;
 
